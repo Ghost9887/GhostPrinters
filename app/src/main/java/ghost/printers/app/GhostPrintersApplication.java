a57cee5acb449f738e7d2547f8138753c6c261cf
Ghost <duchojakub987@gmail.com>
2026-02-18 07:14:15 +0100

    init

---
+++ app/src/main/java/ghost/printers/app/GhostPrintersApplication.java
@@ -0,0 +1,13 @@
+package ghost.printers.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostPrintersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostPrintersApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/ghost/printers/app/GhostPrintersApplicationTests.java
@@ -0,0 +1,13 @@
+package ghost.printers.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostPrintersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
