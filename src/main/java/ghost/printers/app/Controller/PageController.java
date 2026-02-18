07e2eeba519e1da3afe0ec1a4e65599d012d34ae
Ghost <duchojakub987@gmail.com>
2026-02-18 07:30:01 +0100

    first page

---
+++ src/main/java/ghost/printers/app/Controller/PageController.java
@@ -0,0 +1,15 @@
+package ghost.printers.app.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    
+    @GetMapping("/")
+    public String homePage() {
+        return "index.html";
+    }
+
+}
